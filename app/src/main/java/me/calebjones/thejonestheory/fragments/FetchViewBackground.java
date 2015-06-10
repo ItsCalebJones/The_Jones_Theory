11caca0d1ed64eab21afe2ac7db14b9982f84a90
Caleb Jones <ca.jones9119@gmail.com>
2015-06-10 08:46:11 -0400

    Get PostURL and sent to PostSelected

--- app/src/main/java/me/calebjones/thejonestheory/fragments/FetchViewBackground.java
+++ app/src/main/java/me/calebjones/thejonestheory/fragments/FetchViewBackground.java
@@ -126,6 +126,7 @@ public class FetchViewBackground extends Fragment implements SwipeRefreshLayout.
                         intent.putExtra("PostTitle", feedItemList.get(position).getTitle());
                         intent.putExtra("PostImage", feedItemList.get(position).getThumbnail());
                         intent.putExtra("PostText", feedItemList.get(position).getContent());
+                        intent.putExtra("PostURL", feedItemList.get(position).getpostURL());
                         startActivity(intent);
                     }
                 })
@@ -331,6 +332,7 @@ public class FetchViewBackground extends Fragment implements SwipeRefreshLayout.
                 item.setContent(post.optString("content"));
                 item.setExcerpt(post.optString("excerpt"));
                 item.setID(post.optString("ID"));
+                item.setpostURL(post.optString("URL"));
                 Integer ImageLength = post.optString("featured_image").length();
                 if (ImageLength == 0) {
                     Log.d(TAG, "It should be null!");
